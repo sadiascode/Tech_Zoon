22d883657c99bcc3c7634e4b810009dd1d709c85
Sadia Rahman Roha <rahmanrohasadia@gmail.com>
2026-06-08 10:21:55 +0600

    added

---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import '../auth/login_screen.dart';
+
+class TechZoon extends StatelessWidget {
+  const TechZoon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      title: "Tech Zoon",
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/app/app.dart';
+
+
+void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const TechZoon());
+}

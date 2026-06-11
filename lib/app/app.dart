7a5abe1c6709fd5514e0d4f138ab427803aefb59
Sadia Rahman Roha <rahmanrohasadia@gmail.com>
2026-06-11 12:38:04 +0600

    working on products screen

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
 import '../featurs/auth/screen/login_screen.dart';
-import '../featurs/products/provider/cart_provider.dart';
+
 
 class TechZoon extends StatelessWidget {
   const TechZoon({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CartProvider(
-      notifier: CartNotifier(),
-      child: MaterialApp(
+    return MaterialApp(
         title: "Tech Zoon",
         debugShowCheckedModeBanner: false,
         home: LoginScreen(),
-      ),
     );
   }
 }
